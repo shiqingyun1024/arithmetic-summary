1e94dde8d93b2903405295d9bd38e97484b91b6e
814657780 <18037818812@163.com>
2021-06-17 23:07:58 +0800

    AST抽象语法树原理

--- 算法实例/study-ast/src/index.js
+++ 算法实例/study-ast/src/index.js
@@ -1,13 +1,11 @@
 import parse from './parse'
-var templateString = `
-   <div>
+var templateString = `<div>
       <h3>你好</h3>
       <ul>
           <li></li>
           <li></li>
           <li></li>
       </ul>
-   </div>
-`;
+   </div>`;
 parse(templateString)
 console.log(parse(templateString) );
--- 算法实例/study-ast/src/parse.js
+++ 算法实例/study-ast/src/parse.js
@@ -1,4 +1,24 @@
 // parse函数，主函数
-export default function (h){
-    return h
+export default function (templateString){
+    // 指针
+    let index = 0;
+    // 剩余部分
+    let rest = '';
+    // 开始标记
+    const startRegExp = /^\<([a-z]+[1-6]?)\>/;
+    while(index < templateString.length - 1){
+        rest = templateString.slice(index)
+        // console.log(restTemplateString);
+        // console.log(templateString[index]);
+        // 识别遍历剩余的字符。看是不是一个开始标签。
+        if(startRegExp.test(rest)){
+            let tag = rest.match(startRegExp)[1]
+            console.log(tag);
+            // 指针移动标签的长度加2，为什么要加2呢？因为<>也占两位
+            index += tag.toString().length + 2
+        }else{
+            index++
+        }
+        // index++;
+    }
 }
