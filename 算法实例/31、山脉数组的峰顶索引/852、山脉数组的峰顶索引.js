54efd234a5fd18d209d849ea0ed8d43caf3a64a3
814657780 <18037818812@163.com>
2024-08-06 22:41:38 +0800

    算法之山脉数组的峰顶索引

---
+++ 算法实例/31、山脉数组的峰顶索引/852、山脉数组的峰顶索引.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+// 第一种解法
+var peakIndexInMountainArray = function (arr) {
+    let left = 1
+    let right = arr.length - 2
+    while (left <= right) {
+        let mid = left + ((right - left) >> 1)
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+            return mid
+        } else if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) {
+            right = mid - 1
+        } else if (arr[mid] < arr[mid + 1]) {
+            left = mid + 1
+        }
+    }
+};
+
+// 第二种解法
+
+
